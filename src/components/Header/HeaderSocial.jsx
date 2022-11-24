cb490164a5dbb64817242cee458f988aac5a3ea8
mmoale021 <mmoale021@student.wethinkcode.co.za>
2022-11-24 13:39:12 +0200

    hire me button

--- src/components/Header/HeaderSocial.jsx
+++ src/components/Header/HeaderSocial.jsx
@@ -7,8 +7,8 @@ function HeaderSocial() {
   return (
     <div className='header_socials'>
         <a className ='linkedin' href="https://www.linkedin.com/in/mpumelelo-moale-30a879149/" target='_blank'><SlSocialLinkedin size='2rem'/></a>
-        <a className='github' href="https://github.com/mpumi-moale/" target='_blank'><VscGithubAlt size='2rem'/></a>
-        <a className='gitlab' href="https://gitlab.com/moale_mpumi" target='_blank'><FiGitlab size='2rem'/></a>
+        <a className='github' href="https://github.com/mpumi-moale?tab=repositories" target='_blank'><VscGithubAlt size='2rem'/></a>
+        <a className='gitlab' href="https://gitlab.com/dashboard/projects" target='_blank'><FiGitlab size='2rem'/></a>
     </div>
   )
 }
